acbe18144cbed7e53ff042b6e07c4ba960617d63
mehedy3107 <mhador111@gmail.com>
2023-07-16 02:12:51 +0600

    Email matched book update and delete endpoint created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //# Middleware:
 app.use(cors());
@@ -25,27 +25,121 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     client.connect();
-    const productsCollection = client.db("BestReaders").collection("books");
+    const booksCollection = client.db("BestReaders").collection("books");
+    const commentsCollection = client.db("BestReaders").collection("comments");
 
+    //@ Find all books:
     app.get("/all-books", async (req, res) => {
-      const result = await productsCollection.find({}).toArray();
-      res.send(result);
+      const book = await booksCollection.find({}).toArray();
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
     });
 
+    //@ Find all books:
+    app.get("/comments/:id", async (req, res) => {
+      const id = req.params.id;
+      const book = await commentsCollection.find({ id: id }).toArray();
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    });
+
+    //@ Find matched email books:
+    app.get("/matched-books/:email", async (req, res) => {
+      const email = req.params.email;
+      const book = await booksCollection.find({ email }).toArray();
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Can't find any books" });
+      }
+    });
+
+    //@ Find recent added books:
     app.get("/recent-books", async (req, res) => {
-      const result = await productsCollection
+      const book = await booksCollection
         .find({})
         .sort({ timestamp: "descending" })
         .limit(10)
         .toArray();
-      res.send(result);
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
     });
 
+    //@ Find matched with id book:
+    app.get("/singleBook/:id", async (req, res) => {
+      const id = req.params.id;
+      const book = await booksCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    });
+
+    //@ Create new book:
     app.post("/add-book", async (req, res) => {
       const currentTime = new Date();
       req.body.timestamp = currentTime.toISOString();
-      const result = await productsCollection.insertOne(req.body);
-      res.send(result);
+      const book = await booksCollection.insertOne(req.body);
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Can't created new books" });
+      }
+    });
+
+    //@ Create new comment:
+    app.post("/create-comment", async (req, res) => {
+      const currentTime = new Date();
+      req.body.timestamp = currentTime.toISOString();
+      const comment = await commentsCollection.insertOne(req.body);
+      if (comment) {
+        res.status(200).json(comment);
+      } else {
+        res.status(404).json({ message: "Can't post a comment" });
+      }
+    });
+
+    //@ Update matched with email book:
+    app.patch("/update-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+
+      try {
+        const book = await booksCollection.updateOne({ id }, { $set: body });
+
+        if (book) {
+          res.status(200).json(book);
+        } else {
+          res.status(404).json({ message: "Can't update books" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+
+    //@ Update matched with email book:
+    app.delete("/delete-book/:id", async (req, res) => {
+      const id = req.query.id;
+      const book = await booksCollection.deleteOne({
+        id,
+      });
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Can't delete books" });
+      }
     });
   } finally {
     await client.close();
